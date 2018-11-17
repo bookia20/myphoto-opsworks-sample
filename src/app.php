b09ce9c0fa6ef69130a354e818518ec321ae7381
M <you@example.com>
2018-11-17 14:14:12 +1100

    v.01

--- src/app.php
+++ src/app.php
@@ -42,7 +42,7 @@ $app->match('/', function () use ($app) {
     $images = $query->execute() ? $query->fetchAll(PDO::FETCH_ASSOC) : array();
 
     return $app['twig']->render('index.twig', array(
-        'title'  => 'My Photos',
+        'title'  => 'My Photos v0.1',
         'images' => $images,
     ));
 });
